44d2cbb78e6fd28b8fd06bcc8abe565842e590b7
michael dimmitt <michaelgdimmitt@gmail.com>
2019-03-21 16:20:11 -0400

    example JavascriptReadmeResult with an exact copy named SanityTestFile.

---
+++ SanityTestFile.js
@@ -0,0 +1,33 @@
+## Curious in becoming part of the team? (React with React-Router)
+
+<hr/>
+
+![logo - red text](https://user-images.githubusercontent.com/7960991/48717575-b9160180-ebe7-11e8-9786-68980dab412f.png)
+
+<hr/>
+
+
+## Tools we use on the web app at lunchpool:
+1) React,
+2) ReactRouter(nestedRouting),
+3) MaterialUI.
+
+## Install Instructions:
+```
+git clone https://github.com/Lunchpool/lunchpool-react.git &&
+cd lunchpool-react &&
+npm install &&
+npm start
+```
+
+## Additional Instruction if interested in building a different application:
+```
+npm i rename-cli;
+rename lunchpool_web_onboarding your_name_here
+```
+
+## Commands used, if interested:
+```
+npm uninstall -g create-react-app; npm install -g create-react-app; create-react-app react; cd react;
+```
+
